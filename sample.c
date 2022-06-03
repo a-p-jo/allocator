e3d791e7a00394f250ebeb638bee65a2de44707e
A.P. Jo <66941562+a-p-jo@users.noreply.github.com>
2022-06-03 11:06:12 +0530

    v1.3
    Changes `static inline void` function-aliases to `#define`s.
    Fixed StaticAllocator_init() to pass `heap` instead of `&heap` to `allocator_add()`

--- sample.c
+++ sample.c
@@ -1,57 +1,51 @@
 #include "allocator.h"
 
 /* An allocator that uses statically allocated memory */
-static allocator StaticAllocator;
-static void *StaticAllocator_alloc(size_t sz)
-{
-	return allocator_alloc(&StaticAllocator, sz);
-}
-static void StaticAllocator_free(void *p)
-{
-	allocator_free(&StaticAllocator, p);
-}
-static void StaticAllocator_for_blocks(void(*f)(size_t))
-{
-	allocator_for_blocks(&StaticAllocator, f);
-}
-static void StaticAllocator_init(void)
+static allocator sta;
+
+#define sta_alloc(sz)     allocator_alloc(&sta, (sz))
+#define sta_free(p)       allocator_free(&sta, (p))
+#define sta_for_blocks(f) allocator_for_blocks(&sta, (f))
+static void sta_init(void)
 {
 	static max_align_t heap[4096/sizeof(max_align_t)];
-	StaticAllocator = ALLOCATOR_INIT;
-	allocator_add(&StaticAllocator, &heap, sizeof(heap));
+	sta = ALLOCATOR_INIT;
+	allocator_add(&sta, heap, sizeof(heap));
 }
 
-#include <string.h> /* strlen(), strcpy() */
+#include <string.h> /* strlen(), memcpy() */
 #include <stdio.h>  /* puts(), fputs(), stderr */
-#include <assert.h>
+#include <assert.h> /* assert() */
 
 static void pblksz(size_t blksz) {printf("%zu ", blksz);}
 static void pstatus(const char *msg)
 {
 	fputs(msg, stdout);
-	StaticAllocator_for_blocks(pblksz);
+	sta_for_blocks(pblksz);
 	putchar('\n');
 }
 
 int main(int argc, char **argv)
 {
-	StaticAllocator_init();
+	sta_init();
 	pstatus("Initial blocksize(s) : ");
 	
-	/* Allocate and copy argv */
-	char **args = StaticAllocator_alloc(argc * sizeof(char *));
+	/* Allocate and deepcopy argv */
+	char **args = sta_alloc(argc * sizeof(char *));
 	assert(args);
+
 	for (int i = 0; i < argc; i++) {
-		args[i] = StaticAllocator_alloc(strlen(argv[i]));
-		assert(args[i]);
-		strcpy(args[i], argv[i]);
+		size_t len = strlen(argv[i]);
+		assert( (args[i] = sta_alloc(len)) );
+		memcpy(args[i], argv[i], len);
 	}
 	pstatus("Blocksize(s) after allocations : ");
 
-	/* Print the allocated copy and free it */
+	/* Print the allocated copy backwards and free it */
 	while (argc --> 0)
-		puts(args[argc]), StaticAllocator_free(args[argc]);
-	StaticAllocator_free(args);
+		puts(args[argc]), sta_free(args[argc]);
+	
+	sta_free(args);
 	pstatus("Blocksize(s) after freeing : ");
 	return 0;
 }
